5a070efbbdbdbf08efdc965c908215b9ea71d30a
Den <drodsan2708@g.educaand.es>
2023-03-20 13:08:14 +0100

    drodsan2708 - subida de apuntes + ejercicios de clase a GitHub - 20/03/2023

--- src/org/ieslosremedios/daw1/prog/UT6/ejerciciosAlgoritmia/EjercicioBusquedaBiNoDyV.java
+++ src/org/ieslosremedios/daw1/prog/UT6/ejerciciosAlgoritmia/EjercicioBusquedaBiNoDyV.java
@@ -5,19 +5,19 @@ import java.util.Arrays;
 public class EjercicioBusquedaBiNoDyV {
     public static void main(String[] args) {
         int[] a={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-        int parametro=6;
+        int parametro=19;
         int pos=dyv(a,parametro);
         System.out.println("El elemento "+parametro+" se encuentra en la posición "+pos);
 
     }
     private static int dyv(int[] a, int parametro){
-        int laMitad=a.length/2;
-        if(a[laMitad]==parametro){
-            return laMitad;
-        } else if (parametro<a[laMitad]){
-            return dyv(Arrays.copyOfRange(a,0,laMitad-1),parametro);
+        int pos=a.length/2;
+        if(parametro==a[pos]){
+            return pos;
+        } else if (parametro>a[pos]){
+            return pos +1+dyv(Arrays.copyOfRange(a,pos+1,a.length),parametro);
         } else
-            return dyv(Arrays.copyOfRange(a,laMitad+1,a.length),parametro);
+            return dyv(Arrays.copyOfRange(a,0,pos),parametro);
 
 
     }
